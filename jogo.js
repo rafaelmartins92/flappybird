39e04d6e04ed53c2b2fe5141522dc63edb084513
Rafael Martins <rafael.martins92@outlook.com>
2021-01-18 23:07:59 -0300

    feat: add screens change

--- jogo.js
+++ jogo.js
@@ -106,16 +106,71 @@ const flappyBird = {
   },
 };
 
+// [mensagemGetReady]
+const mensagemGetReady = {
+  sX: 134,
+  sY: 0,
+  w: 174,
+  h: 152,
+  x: canvas.width / 2 - 174 / 2,
+  y: 50,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      mensagemGetReady.sX,
+      mensagemGetReady.sY,
+      mensagemGetReady.w,
+      mensagemGetReady.h,
+      mensagemGetReady.x,
+      mensagemGetReady.y,
+      mensagemGetReady.w,
+      mensagemGetReady.h
+    );
+  },
+};
+
+// [Telas]
+let telaAtiva = {};
+function mudaParaTela(novaTela) {
+  telaAtiva = novaTela;
+}
+const Telas = {
+  INICIO: {
+    desenha() {
+      planoDeFundo.desenha();
+      chao.desenha();
+      flappyBird.desenha();
+      mensagemGetReady.desenha();
+    },
+    click() {
+      mudaParaTela(Telas.JOGO);
+    },
+    atualiza() {},
+  },
+};
+
+Telas.JOGO = {
+  desenha() {
+    planoDeFundo.desenha();
+    chao.desenha();
+    flappyBird.desenha();
+  },
+  atualiza() {
+    flappyBird.atualiza();
+  },
+};
+
 function loop() {
-  flappyBird.atualiza();
-
-  planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
-
-  flappyBird.y = flappyBird.y + 1;
+  telaAtiva.desenha();
+  telaAtiva.atualiza();
 
   requestAnimationFrame(loop);
 }
 
+window.addEventListener("click", function () {
+  if (telaAtiva.click) {
+    telaAtiva.click();
+  }
+});
+mudaParaTela(Telas.INICIO);
 loop();
